75deab842a8804a51ed491e69e678a58ab526f28
Morten Friesgaard <friesgaard@gmail.com>
2017-03-31 16:33:32 +0200

    changed colors

--- src/styles/index.js
+++ src/styles/index.js
@@ -10,13 +10,17 @@ const colorDef = {
   darkGrey: '#2F2F2F',
   grey: '#DDDDDD',
   lightGrey: '#F2F2F2',
+  entur: {
+    primary: '#273A46',   // 39,58,70
+    secondary: '#6BBDC2', //107,189,194,
+  }
 }
 
 export const color = {
   font: {
     inverse: colorDef.white,
     title: colorDef.black,
-    info1: colorDef.bigblue,
+    info1: colorDef.entur.secondary,
     info2: colorDef.darkBlue,
     info3: colorDef.black,
     warn: colorDef.red,
@@ -24,14 +28,14 @@ export const color = {
 
     tooltip: colorDef.white,
   },
-  background: colorDef.darkGrey,
+  background: colorDef.entur.primary,
   border: colorDef.black,
   effective: colorDef.black,
   fail: colorDef.red,
   modal: colorDef.white,
   backdrop: 'rgba(0, 0, 0, 0.3)',
   tooltip: '#191919',
-  tabActive: colorDef.red,
+  tabActive: colorDef.entur.secondary,
 
   timeLineBackground: '#DED8D8',
   timeLineBlockBackground: '#6D92B6',
