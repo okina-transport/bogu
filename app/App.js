8b6ab2377022bbe1b4ea12f9a13a3d7e056fa1bb
Morten Friesgaard <friesgaard@gmail.com>
2017-04-20 07:48:51 +0200

    adds styling from bel

--- app/App.js
+++ app/App.js
@@ -27,29 +27,31 @@ class App extends Component {
     return (
       <div>
         <h1>HeaderTimeline test</h1>
-        {
-          order.map( (line, index) => (
-            <div>
-              <HeaderTimeline line={line}
-                              hoverText={linesMap[line].lineNames.join(', ')}
-                              index={index}
-                              key={'HeaderTimeline'+index}
-                              validDaysOffset={validDaysOffset}
-                              validFromDate={validFromDate}
-                              effectivePeriods={linesMap[line].effectivePeriods}/>
-              {
-                linesMap[line].lines.map( (l, i) => (
-                  <Timeline
-                    key={'timelineItem'+index+'-'+i}
-                    timetables={l.timetables}
-                    isLast={i === linesMap[line].lines.length-1}
-                    validDaysOffset={validDaysOffset}
-                  />
-                ))
-              }
-            </div>
-          ))
-        }
+        <div style={{overflowY: 'scroll', overflowX: 'hidden', margin: 'auto', width: '100%'}}>
+          {
+            order.map( (line, index) => (
+              <div style={{padding: 0, marginLeft: 0, marginRight: 0, marginTop: 0, lineHeight: 0}}>
+                <HeaderTimeline line={line}
+                                hoverText={linesMap[line].lineNames.join(', ')}
+                                index={index}
+                                key={'HeaderTimeline'+index}
+                                validDaysOffset={validDaysOffset}
+                                validFromDate={validFromDate}
+                                effectivePeriods={linesMap[line].effectivePeriods}/>
+                {
+                  linesMap[line].lines.map( (l, i) => (
+                    <Timeline
+                      key={'timelineItem'+index+'-'+i}
+                      timetables={l.timetables}
+                      isLast={i === linesMap[line].lines.length-1}
+                      validDaysOffset={validDaysOffset}
+                    />
+                  ))
+                }
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
   }
