01a6b5f2760c4bd10e9d9241c7495611f03ecb0a
Sean Scully <sean.erik.scully@gmail.com>
2017-07-28 14:35:26 +0200

    NRP-1874 fixes bug with active filter for event not showing correctly

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,122 +1,171 @@
-import React, { Component, PropTypes } from 'react'
-import EventStepper from './EventStepper'
-require('./EventDetails.css')
-const FaFresh = require('react-icons/lib/fa/refresh')
-import translations from './translations'
+import React, { Component, PropTypes } from 'react';
+import EventStepper from './EventStepper';
+require('./EventDetails.css');
+const FaFresh = require('react-icons/lib/fa/refresh');
+import translations from './translations';
 
 class EventDetails extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       activePageIndex: 0,
-      endStateFilter: "ALL",
-    }
+      endStateFilter: 'ALL'
+    };
   }
 
-  handlePageClick (e, pageIndex) {
-    e.preventDefault()
+  handlePageClick(e, pageIndex) {
+    e.preventDefault();
     this.setState({
       activePageIndex: pageIndex
-    })
+    });
   }
 
   render() {
 
-    const { dataSource, locale, includeLevel2 } = this.props
-    const { activePageIndex, endStateFilter } = this.state
+    const { dataSource, locale, includeLevel2 } = this.props;
+    const { activePageIndex, endStateFilter } = this.state;
 
-    let filteredPages = null
+    let filteredPages = null;
 
-    if (endStateFilter === "FAILED" || endStateFilter === "OK") {
-      filteredPages = dataSource.filter( event => event.endState === endStateFilter )
+    if (endStateFilter === 'FAILED' || endStateFilter === 'OK') {
+      filteredPages = dataSource.filter(
+        event => event.endState === endStateFilter
+      );
     }
 
-    let paginationMap = getPaginationMap(filteredPages || dataSource)
+    let paginationMap = getPaginationMap(filteredPages || dataSource);
 
-    const page = paginationMap[activePageIndex]
+    const page = paginationMap[activePageIndex];
 
-    const refreshButton = this.props.handleRefresh ? (
-      <div style={{marginRight: 15, float: 'right', cursor: 'pointer'}}><FaFresh style={{transform: 'scale(1.5)'}} onClick={this.props.handleRefresh}/></div>
-    ) : null
+    const refreshButton = this.props.handleRefresh
+      ? <div style={{ marginRight: 15, float: 'right', cursor: 'pointer' }}>
+          <FaFresh
+            style={{ transform: 'scale(1.5)' }}
+            onClick={this.props.handleRefresh}
+          />
+        </div>
+      : null;
 
     if (page && page.length && paginationMap) {
-
       return (
         <div>
-          <div style={{width: '100%', textAlign: 'left', marginBottom: 5}}>
-            <select onChange={ e => { this.setState({endStateFilter: e.target.value, activePageIndex: 0}) }}>
+          <div style={{ width: '100%', textAlign: 'left', marginBottom: 5 }}>
+            <select
+              value={endStateFilter}
+              onChange={e => {
+                this.setState({
+                  endStateFilter: e.target.value,
+                  activePageIndex: 0
+                });
+              }}
+            >
               <option value="ALL">{translations[locale].show_all}</option>
-              <option value="OK">{translations[locale].show_only_success}</option>
-              <option value="FAILED">{translations[locale].show_only_failed}</option>
+              <option value="OK">
+                {translations[locale].show_only_success}
+              </option>
+              <option value="FAILED">
+                {translations[locale].show_only_failed}
+              </option>
             </select>
           </div>
-          { refreshButton}
+          {refreshButton}
           <div className="page-link-parent">
             <span>{translations[locale].page}</span>
-            { paginationMap.map ( (page, index) => {
-              const isActive = (index == activePageIndex) ? 'page-link active-link' : 'page-link inactive-link'
+            {paginationMap.map((page, index) => {
+              const isActive = index == activePageIndex
+                ? 'page-link active-link'
+                : 'page-link inactive-link';
               return (
                 <span
                   className={isActive}
-                  onClick={(e) => this.handlePageClick(e, index)}
-                  key={"link-" + index}>
-                  { index+1 }
-                  </span>
-              )
+                  onClick={e => this.handlePageClick(e, index)}
+                  key={'link-' + index}
+                >
+                  {index + 1}
+                </span>
+              );
             })}
           </div>
           <div>
-            { page.map ( (listItem, index) => {
+            {page.map((listItem, index) => {
+              let eventGroup = {};
 
-              let eventGroup = {}
-
-              listItem.events.forEach( event => {
-
+              listItem.events.forEach(event => {
                 if (!eventGroup[event.action]) {
-                  eventGroup[event.action] = {}
-                  eventGroup[event.action].states = []
+                  eventGroup[event.action] = {};
+                  eventGroup[event.action].states = [];
                 }
-                eventGroup[event.action].states.push(event)
-                eventGroup[event.action].endState = event.state
-              })
+                eventGroup[event.action].states.push(event);
+                eventGroup[event.action].endState = event.state;
+              });
 
               return (
-                <div key={"jobstatus-" + listItem.chouetteJobId + '-' + index} style={{marginBottom: 20, border: '1px solid #eee', padding: 10}}>
-                  <EventStepper includeLevel2={includeLevel2} locale={locale} key={"event-group-" + listItem.chouetteJobId + '-' + index} groups={eventGroup} listItem={listItem}/>
+                <div
+                  key={'jobstatus-' + listItem.chouetteJobId + '-' + index}
+                  style={{
+                    marginBottom: 20,
+                    border: '1px solid #eee',
+                    padding: 10
+                  }}
+                >
+                  <EventStepper
+                    includeLevel2={includeLevel2}
+                    locale={locale}
+                    key={'event-group-' + listItem.chouetteJobId + '-' + index}
+                    groups={eventGroup}
+                    listItem={listItem}
+                  />
                 </div>
-              )
-            }) }
+              );
+            })}
           </div>
         </div>
-      )
-
+      );
     } else {
       return (
-      <div style={{width: '100%', textAlign: 'left', marginBottom: 5}}>
-        <select onChange={ e => { this.setState({endStateFilter: e.target.value, activePageIndex: 0}) }}>
-          <option value="ALL">{translations[locale].show_all}</option>
-          <option value="OK">{translations[locale].show_only_success}</option>
-          <option value="FAILED">{translations[locale].show_only_failed}</option>
-        </select>
-        <div style={{marginBottom: 20, marginTop: 20, border: '1px solid #eee', padding: 10}}>
-          <div style={{fontWeight: 600}}>{translations[locale].no_status}</div>
+        <div style={{ width: '100%', textAlign: 'left', marginBottom: 5 }}>
+          <select
+            value={endStateFilter}
+            onChange={e => {
+              this.setState({
+                endStateFilter: e.target.value,
+                activePageIndex: 0
+              });
+            }}
+          >
+            <option value="ALL">{translations[locale].show_all}</option>
+            <option value="OK">{translations[locale].show_only_success}</option>
+            <option value="FAILED">
+              {translations[locale].show_only_failed}
+            </option>
+          </select>
+          <div
+            style={{
+              marginBottom: 20,
+              marginTop: 20,
+              border: '1px solid #eee',
+              padding: 10
+            }}
+          >
+            <div style={{ fontWeight: 600 }}>
+              {translations[locale].no_status}
+            </div>
+          </div>
         </div>
-      </div>
-      )
+      );
     }
   }
 }
 
 const getPaginationMap = (statusList = []) => {
-  let paginationMap = []
+  let paginationMap = [];
 
   if (statusList && statusList.length) {
-    for (let i = 0, j = statusList.length; i < j; i+=10) {
-      paginationMap.push(statusList.slice(i,i+10))
+    for (let i = 0, j = statusList.length; i < j; i += 10) {
+      paginationMap.push(statusList.slice(i, i + 10));
     }
   }
-  return paginationMap
-}
+  return paginationMap;
+};
 
-export default EventDetails
+export default EventDetails;
