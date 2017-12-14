ae33230463cb39778240c3d119cdff3f544dcc50
Sean Scully <sean.erik.scully@gmail.com>
2017-12-14 01:21:13 +0100

    ROR-32 Re-format code, excuses new tag due to previous incorrect unstages build files => v. 0.6.0

--- src/components/ControlledChouetteLink.js
+++ src/components/ControlledChouetteLink.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import ChouetteLink from './ChouetteLink'
+import React from 'react';
+import ChouetteLink from './ChouetteLink';
 
 class ControlledChouetteLink extends React.Component {
-
   render() {
+    const { events, includeLevel2 } = this.props;
 
-    const { events, includeLevel2 } = this.props
-
-    let supportedActions = ['IMPORT', 'VALIDATION_LEVEL_1']
+    let supportedActions = ['IMPORT', 'VALIDATION_LEVEL_1'];
 
     let choueteActionMap = {
-      'IMPORT': 'importer',
-      'VALIDATION_LEVEL_1': 'validator',
-    }
+      IMPORT: 'importer',
+      VALIDATION_LEVEL_1: 'validator'
+    };
 
     if (includeLevel2) {
-      supportedActions.push('VALIDATION_LEVEL_2')
-      supportedActions.push('EXPORT')
-      supportedActions.push('EXPORT_NETEX')
-      choueteActionMap['VALIDATION_LEVEL_2'] = 'validator'
-      choueteActionMap['EXPORT'] = 'exporter'
-      choueteActionMap['EXPORT_NETEX'] = 'exporter'
+      supportedActions.push('VALIDATION_LEVEL_2');
+      supportedActions.push('EXPORT');
+      supportedActions.push('EXPORT_NETEX');
+      choueteActionMap['VALIDATION_LEVEL_2'] = 'validator';
+      choueteActionMap['EXPORT'] = 'exporter';
+      choueteActionMap['EXPORT_NETEX'] = 'exporter';
     }
 
     if (events.states && events.states.length) {
-      const endState = events.states[events.states.length-1]
+      const endState = events.states[events.states.length - 1];
       if (supportedActions.indexOf(endState.action) > -1) {
         return (
           <ChouetteLink
@@ -32,15 +30,15 @@ class ControlledChouetteLink extends React.Component {
             id={endState.chouetteJobId}
             referential={endState.referential}
           >
-            { this.props.children }
+            {this.props.children}
           </ChouetteLink>
-        )
+        );
       }
     }
 
-
-    return <div> { this.props.children } </div>
+    return <div> {this.props.children}
+      {' '}</div>;
   }
 }
 
-export default ControlledChouetteLink
+export default ControlledChouetteLink;
